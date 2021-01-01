c8d0785303dc441fe825b48e28b5fd4964b5dc54
Tomasz Nowak <46384146+TommyRBLX@users.noreply.github.com>
2021-01-01 02:43:48 +0100

    Update mathex.lua

--- mathex.lua
+++ mathex.lua
@@ -66,38 +66,6 @@ end
 
 
 -- // Lerp functions
---[[
- //Ease out
-    public static float Sinerp(float start, float end, float value)
-    {
-        return Mathf.Lerp(start, end, Mathf.Sin(value * Mathf.PI * 0.5f));
-    }
- 
-    public static Vector2 Sinerp(Vector2 start, Vector2 end, float value)
-    {
-        return new Vector2(Mathf.Lerp(start.x, end.x, Mathf.Sin(value * Mathf.PI * 0.5f)), Mathf.Lerp(start.y, end.y, Mathf.Sin(value * Mathf.PI * 0.5f)));
-    }
- 
-    public static Vector3 Sinerp(Vector3 start, Vector3 end, float value)
-    {
-        return new Vector3(Mathf.Lerp(start.x, end.x, Mathf.Sin(value * Mathf.PI * 0.5f)), Mathf.Lerp(start.y, end.y, Mathf.Sin(value * Mathf.PI * 0.5f)), Mathf.Lerp(start.z, end.z, Mathf.Sin(value * Mathf.PI * 0.5f)));
-    }
-    //Ease in
-    public static float Coserp(float start, float end, float value)
-    {
-        return Mathf.Lerp(start, end, 1.0f - Mathf.Cos(value * Mathf.PI * 0.5f));
-    }
- 
-    public static Vector2 Coserp(Vector2 start, Vector2 end, float value)
-    {
-        return new Vector2(Coserp(start.x,end.x,value),Coserp(start.y,end.y,value));
-    }
- 
-    public static Vector3 Coserp(Vector3 start, Vector3 end, float value)
-    {
-        return new Vector3(Coserp(start.x, end.x, value), Coserp(start.y, end.y, value), Coserp(start.z, end.z, value));
-    }
-]]
 
 -- Simple lerp
 mathex.lerp = function(a,b,t)
